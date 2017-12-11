c9bbc1ae40c910ae0d31215bda82c6b8d4df645b
David Mallard <correspondence@davidmallard.id.au>
2017-12-11 14:49:19 +1100

    Use a FlatList to support scrolling on the deck list view

--- components/DeckList.js
+++ components/DeckList.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { receiveDecks } from '../actions';
 import { getDecks, resetDecks } from '../utils/api';
-import { View, Button } from 'react-native';
+import { FlatList, View, Button } from 'react-native';
 import { List, ListItem } from 'react-native-elements';
 
 class DeckList extends Component<{}, void> {
@@ -23,38 +23,49 @@ class DeckList extends Component<{}, void> {
     getDecks().then(decks => this.props.receiveDecks(decks));
   };
 
+  // FlatList ideas via https://medium.com/react-native-development/how-to-use-the-flatlist-component-react-native-basics-92c482816fe6
+  renderItem = ({ item }) => {
+    const cardCount = item.questions.length; // Get the number of questions
+    // Generate last quiz date string while handling null values
+    const lastQuiz = item.lastQuizDate
+      ? new Date(item.lastQuizDate).toLocaleDateString()
+      : 'never';
+
+    return (
+      <ListItem
+        key={item.title}
+        title={item.title}
+        subtitle={`Last quiz completed: ${lastQuiz}`}
+        badge={{
+          value: cardCount,
+          containerStyle: { marginTop: 10 }
+        }}
+        onPress={() =>
+          this.props.navigation.navigate('Deck', { key: item.title })
+        }
+      />
+    );
+  };
+
+  renderFooter = () => {
+    return (
+      <Button title="Reset Sample Data" onPress={() => this.resetSubmit()} />
+    );
+  };
+
   render() {
     const { decks } = this.props;
 
     return (
       <View>
         <List>
-          {decks &&
-            Object.keys(decks).map(key => {
-              const deck = decks[key]; // Get the deck object matching the key
-              const cardCount = deck.questions.length; // Get the number of questions
-              // Generate last quiz date string while handling null values
-              const lastQuiz = deck.lastQuizDate
-                ? new Date(deck.lastQuizDate).toLocaleDateString()
-                : 'never';
-
-              return (
-                <ListItem
-                  key={key}
-                  title={deck.title}
-                  subtitle={`Last quiz completed: ${lastQuiz}`}
-                  badge={{
-                    value: cardCount,
-                    containerStyle: { marginTop: 10 }
-                  }}
-                  onPress={() =>
-                    this.props.navigation.navigate('Deck', { key: key })
-                  }
-                />
-              );
-            })}
+          <FlatList
+            data={Object.values(decks)}
+            renderItem={this.renderItem}
+            ListFooterComponent={this.renderFooter}
+            keyExtractor={item => item.title}
+          />
         </List>
-        <Button title="Reset Sample Data" onPress={() => this.resetSubmit()} />
       </View>
     );
   }
